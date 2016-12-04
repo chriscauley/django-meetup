4068c167d92172144bcb78c76ffdf52c84ec0ae7
chriscauley <chris@lablackey.com>
2016-12-04 18:35:22 -0500

    initial app

---
+++ main/urls.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.conf.urls import include, url
+
+from django.contrib import admin
+from django.contrib.auth import urls as auth_urls
+from django.views.static import serve
+from main import views as main_views
+import meetup.views
+
+admin.autodiscover()
+
+urlpatterns = [
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^auth/',include(auth_urls)),
+
+  url(r'^$', main_views.home,name='home'),
+  url(r'favicon.ico$', main_views.redirect,
+      {'url': getattr(settings,'FAVICON','/static/favicon.png')}),
+  url(r'^app.appcache$',main_views.direct_to_template,{'template': 'app.appcache'}),
+  url(r'^group/([^/]+)/$',meetup.views.view_group),
+]
+
+if settings.DEBUG:
+  urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+  ]
